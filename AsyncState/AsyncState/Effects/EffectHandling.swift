6eaee714fcf70d36a43eb1f296e044c8ea739e07
Pete <pschuette22@gmail.com>
2024-06-14 10:57:41 -0500

    effect batch handling

--- AsyncState/AsyncState/Effects/EffectHandling.swift
+++ AsyncState/AsyncState/Effects/EffectHandling.swift
@@ -12,8 +12,12 @@ public protocol EffectHandling<HandledEffect>: AnyObject {
     associatedtype HandledEffect: Effect
 
     /// Handler declaration for a given effect. This is not called from a specific thread and syncronization is an obligation of the implementer
-    /// - Parameter effect: Effect
+    /// - Parameter effect: ``Effect``
     func handle(_ effect: HandledEffect)
+
+    /// Handler declaration for a batch of effects received at the same time.
+    /// - Parameter effects: ``Array`` of ``Effect``s which are handled by this object
+    func handle(all effects: [HandledEffect])
 }
 
 extension EffectHandling where Self: EffectMapping {
@@ -22,6 +26,14 @@ extension EffectHandling where Self: EffectMapping {
             guard let mappedEffect = await self?.map(effect) else { return }
 
             self?.handle(mappedEffect)
+        }
+    }
+
+    func handleIfNeeded(_ effects: [any Effect]) {
+        Task { [weak self] in
+            guard let mappedEffects = await self?.map(all: effects) else { return }
+
+            self?.handle(all: mappedEffects)
         }
     }
 }
