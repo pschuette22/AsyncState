6e019f8674fe9ce6e1efedc40420355f85fec174
Pete <pschuette22@gmail.com>
2024-06-14 16:52:39 -0500

    lint + format

--- AsyncState/AsyncState/Events/EventReceiving.swift
+++ AsyncState/AsyncState/Events/EventReceiving.swift
@@ -10,16 +10,44 @@ import Foundation
 public protocol EventReceiving: EffectHandling {}
 
 extension EventReceiving {
-    func receive<StreamedEvent>(
-        eventsFrom eventStreamer: some EventStreaming<StreamedEvent>,
-        _ mapping: @escaping (StreamedEvent) -> [HandledEffect]
+    /// Receive ``Event``s from a streaming object and translate them into ``Effect``s
+    /// - Parameters:
+    ///   - eventStreamer: Object streaming ``Event``s
+    ///   - effectMapping: ``Effect`` these events have
+    func receive<ReceivedEvent>(
+        from eventStreamer: some EventStreaming<ReceivedEvent>,
+        _ effectMapping: @escaping (ReceivedEvent) -> [HandledEffect]
     ) {
         Task { [weak self, weak eventStreamer] in
             guard var iterator = eventStreamer?.eventStream.observe().makeAsyncIterator() else { return }
             while let event = await iterator.next() {
-                let effects = mapping(event)
+                let effects = effectMapping(event)
                 if effects.isEmpty { continue }
                 self?.handle(all: effects)
+            }
+        }
+    }
+}
+
+extension EventReceiving where Self: EventStreaming {
+    func receive<ReceivedEvent>(
+        from eventStreamer: some EventStreaming<ReceivedEvent>,
+        rebroadcasting eventMapping: @escaping (ReceivedEvent) -> StreamedEvent?,
+        _ effectMapping: @escaping (ReceivedEvent) -> [HandledEffect]
+    ) {
+        Task { [weak self, weak eventStreamer] in
+            guard var iterator = eventStreamer?.eventStream.observe().makeAsyncIterator() else { return }
+            while let event = await iterator.next() {
+                //
+                if let rebroadcasted = eventMapping(event) {
+                    // This is a little funky. Should we keep the send function open?
+                    (self?.eventStream as? OpenAsyncBroadcast<StreamedEvent>)?.send(rebroadcasted)
+                }
+
+                let effects = effectMapping(event)
+                if !effects.isEmpty {
+                    self?.handle(all: effects)
+                }
             }
         }
     }
