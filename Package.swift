7323343971d04581e716575197bb2a661a05a44a
Pete <pschuette22@gmail.com>
2024-06-17 10:48:21 -0500

    refactor for SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "async-state",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "async-state",
+            targets: ["AsyncState"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AsyncState"),
+        .testTarget(
+            name: "AsyncStateTests",
+            dependencies: ["AsyncState"]
+        ),
+    ]
+)
