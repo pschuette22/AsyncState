ee349513b7809beb9ff724d035ed5dfab32fc12b
Pete <pschuette22@gmail.com>
2024-08-16 12:16:44 -0500

    cell templates

---
+++ xctemplates/Configured Supplementary View.xctemplate/___FILEBASENAME___SupplementaryView.swift
@@ -0,0 +1,41 @@
+// ___FILEHEADER___
+
+import AsyncState
+import Foundation
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: UICollectionReusableView, ConfigurableSupplementaryView {
+    static let reuseIdentifier = "\(Bundle.main.bundleIdentifier ?? "").___FILEBASENAMEASIDENTIFIER___"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+       // initialize what is needed
+    }
+
+    @available(*, unavailable, message: "Storyboards are not supported. Use ``init(frame:)``")    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // reset the cell
+    }
+}
+
+// MARK: - Subview
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    // TODO: Define transactions
+    struct Configuration: ViewConfiguration {
+        
+    }
+
+    func setupSubviews() {
+        // TODO: Setup Subviews
+    }
+
+    func apply(_ configuration: Configuration) {
+        // Apply the layout configuration
+    }
+}
