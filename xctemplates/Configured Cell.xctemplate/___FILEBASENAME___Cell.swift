296b30467540e0263190767e22e4ffb6e8449c4c
Pete <pschuette22@gmail.com>
2024-08-16 12:18:30 -0500

    [FEATURE] Cell Templates (#12)
    * cell templates
    
    * template descriptions

---
+++ xctemplates/Configured Cell.xctemplate/___FILEBASENAME___Cell.swift
@@ -0,0 +1,40 @@
+// ___FILEHEADER___
+
+import AsyncState
+import Foundation
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: UICollectionViewCell, ConfigurableCell {
+    static let reuseIdentifier = "\(Bundle.main.bundleIdentifier ?? "").___FILEBASENAMEASIDENTIFIER___"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+    }
+
+    @available(*, unavailable, message: "Storyboards are not supported. Use ``init(frame:)``")    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // reset the cell
+    }
+}
+
+// MARK: - Subview
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    // TODO: Define transactions
+    struct Configuration: ViewConfiguration {
+        
+    }
+
+    func setupSubviews() {
+        // TODO: Setup Subviews
+    }
+
+    func apply(_ configuration: Configuration) {
+        // TODO: Apply the layout configuration
+    }
+}
